ba6eea1a5042f01ea567513d986e989f415f6728
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2026-03-29 17:46:58 +0530

    Set PM2 instances to 2 for all microservices

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,20 +4,23 @@ module.exports = {
     script     : "java -jar ./stock-service/target/stock-service-0.0.1-SNAPSHOT.jar",
     out_file   : "./logs/stock-service.log",
     error_file : "./logs/stock-service-error.log",
-    merge_logs : true
+    merge_logs : true,
+    instances  : 2
   },
   {
     name       : "supplier-service",
     script     : "java -jar ./supplier-service/target/supplier-service-0.0.1-SNAPSHOT.jar",
     out_file   : "./logs/supplier-service.log",
     error_file : "./logs/supplier-service-error.log",
-    merge_logs : true
+    merge_logs : true,
+    instances  : 2
   },
   {
     name       : "file-service",
     script     : "java -jar ./file-service/target/file-service-0.0.1-SNAPSHOT.jar",
     out_file   : "./logs/file-service.log",
     error_file : "./logs/file-service-error.log",
-    merge_logs : true
+    merge_logs : true,
+    instances  : 2
   }]
 }
